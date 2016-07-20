6c16f95aa8a47f80695a21625e74ef4961b7e688
RajeebTheGreat <rajeebthegreat@gmail.com>
2016-07-20 11:17:01 +0545

    Adds Compare Table in about page

--- inc/admin/class-envince-admin.php
+++ inc/admin/class-envince-admin.php
@@ -4,7 +4,7 @@
  *
  * @author  ThemeGrill
  * @package envince
- * @since   1.3.8
+ * @since   1.1.3
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
@@ -23,6 +23,7 @@ class Envince_Admin {
 	 */
 	public function __construct() {
 		add_action( 'admin_menu', array( $this, 'admin_menu' ) );
+		add_action( 'wp_loaded', array( __CLASS__, 'hide_notices' ) );
 		add_action( 'load-themes.php', array( $this, 'admin_notice' ) );
 	}
 
@@ -49,10 +50,34 @@ class Envince_Admin {
 	 * Add admin notice.
 	 */
 	public function admin_notice() {
-		global $pagenow;
+		global $envince_version, $pagenow;
 
+		wp_enqueue_style( 'envince-message', get_template_directory_uri() . '/css/admin/message.css', array(), $envince_version );
+
+
+		// Let's bail on theme activation.
 		if ( 'themes.php' == $pagenow && isset( $_GET['activated'] ) ) {
 			add_action( 'admin_notices', array( $this, 'welcome_notice' ) );
+			update_option( 'envince_admin_notice_welcome', 1 );
+		// No option? Let run the notice wizard again..
+		} elseif( ! get_option( 'envince_admin_notice_welcome' ) ) {
+			add_action( 'admin_notices', array( $this, 'welcome_notice' ) );
+		}
+	}
+
+	/**
+	 * Hide a notice if the GET variable is set.
+	 */
+	public static function hide_notices() {
+		if ( isset( $_GET['envince-hide-notice'] ) && isset( $_GET['_envince_notice_nonce'] ) ) {
+			if ( ! wp_verify_nonce( $_GET['_envince_notice_nonce'], 'envince_hide_notices_nonce' ) ) {
+				wp_die( __( 'Action failed. Please refresh the page and retry.', 'envince' ) );
+			}
+			if ( ! current_user_can( 'manage_options' ) ) {
+				wp_die( __( 'Cheatin&#8217; huh?', 'envince' ) );
+			}
+			$hide_notice = sanitize_text_field( $_GET['envince-hide-notice'] );
+			update_option( 'envince_admin_notice_' . $hide_notice, 1 );
 		}
 	}
 
@@ -61,9 +86,12 @@ class Envince_Admin {
 	 */
 	public function welcome_notice() {
 		?>
-		<div class="updated notice is-dismissible">
-			<p><?php echo sprintf( esc_html__( 'Welcome! Thank you for choosing Envince! To fully take advantage of the best our theme can offer please make sure you visit our %swelcome page%s.', 'envince' ), '<a href="' . esc_url( admin_url( 'themes.php?page=envince-welcome' ) ) . '">', '</a>' ); ?></p>
-			<p><a href="<?php echo esc_url( admin_url( 'themes.php?page=envince-welcome' ) ); ?>" class="button" style="text-decoration: none;"><?php esc_html_e( 'Get started with Envince', 'envince' ); ?></a></p>
+		<div id="message" class="updated envince-message">
+			<a class="envince-message-close notice-dismiss" href="<?php echo esc_url( wp_nonce_url( remove_query_arg( array( 'activated' ), add_query_arg( 'envince-hide-notice', 'welcome' ) ), 'envince_hide_notices_nonce', '_envince_notice_nonce' ) ); ?>"><?php _e( 'Dismiss', 'envince' ); ?></a>
+			<p><?php printf( esc_html__( 'Welcome! Thank you for choosing Envince! To fully take advantage of the best our theme can offer please make sure you visit our %swelcome page%s.', 'envince' ), '<a href="' . esc_url( admin_url( 'themes.php?page=envince-welcome' ) ) . '">', '</a>' ); ?></p>
+			<p class="submit">
+				<a class="button-secondary" href="<?php echo esc_url( admin_url( 'themes.php?page=envince-welcome' ) ); ?>"><?php esc_html_e( 'Get started with Envince', 'envince' ); ?></a>
+			</p>
 		</div>
 		<?php
 	}
@@ -101,6 +129,8 @@ class Envince_Admin {
 
 			<a href="<?php echo esc_url( apply_filters( 'envince_pro_theme_url', 'http://demo.themegrill.com/envince/' ) ); ?>" class="button button-secondary docs" target="_blank"><?php esc_html_e( 'View Demo', 'envince' ); ?></a>
 
+			<a href="<?php echo esc_url( apply_filters( 'envince_pro_theme_url', 'http://themegrill.com/themes/envince/' ) ); ?>" class="button button-primary docs" target="_blank"><?php esc_html_e( 'View Pro', 'envince' ); ?></a>
+
 			<a href="<?php echo esc_url( apply_filters( 'envince_pro_theme_url', 'http://wordpress.org/support/view/theme-reviews/envince?filter=5' ) ); ?>" class="button button-secondary docs" target="_blank"><?php esc_html_e( 'Rate this theme', 'envince' ); ?></a>
 		</p>
 
@@ -110,6 +140,9 @@ class Envince_Admin {
 			</a>
 			<a class="nav-tab <?php if ( isset( $_GET['tab'] ) && $_GET['tab'] == 'supported_plugins' ) echo 'nav-tab-active'; ?>" href="<?php echo esc_url( admin_url( add_query_arg( array( 'page' => 'envince-welcome', 'tab' => 'supported_plugins' ), 'themes.php' ) ) ); ?>">
 				<?php esc_html_e( 'Supported Plugins', 'envince' ); ?>
+			</a>
+			<a class="nav-tab <?php if ( isset( $_GET['tab'] ) && $_GET['tab'] == 'free_vs_pro' ) echo 'nav-tab-active'; ?>" href="<?php echo esc_url( admin_url( add_query_arg( array( 'page' => 'envince-welcome', 'tab' => 'free_vs_pro' ), 'themes.php' ) ) ); ?>">
+				<?php esc_html_e( 'Free Vs Pro', 'envince' ); ?>
 			</a>
 			<a class="nav-tab <?php if ( isset( $_GET['tab'] ) && $_GET['tab'] == 'changelog' ) echo 'nav-tab-active'; ?>" href="<?php echo esc_url( admin_url( add_query_arg( array( 'page' => 'envince-welcome', 'tab' => 'changelog' ), 'themes.php' ) ) ); ?>">
 				<?php esc_html_e( 'Changelog', 'envince' ); ?>
@@ -146,25 +179,31 @@ class Envince_Admin {
 			<div class="changelog point-releases">
 				<div class="under-the-hood two-col">
 					<div class="col">
-						<h3><?php echo esc_html_e( 'Theme Customizer', 'envince' ); ?></h3>
+						<h3><?php esc_html_e( 'Theme Customizer', 'envince' ); ?></h3>
 						<p><?php esc_html_e( 'All Theme Options are available via Customize screen.', 'envince' ) ?></p>
 						<p><a href="<?php echo admin_url( 'customize.php' ); ?>" class="button button-secondary"><?php esc_html_e( 'Customize', 'envince' ); ?></a></p>
 					</div>
 
 					<div class="col">
-						<h3><?php echo esc_html_e( 'Documentation', 'envince' ); ?></h3>
+						<h3><?php esc_html_e( 'Documentation', 'envince' ); ?></h3>
 						<p><?php esc_html_e( 'Please view our documentation page to setup the theme.', 'envince' ) ?></p>
 						<p><a href="<?php echo esc_url( 'http://docs.themegrill.com/envince/' ); ?>" class="button button-secondary"><?php esc_html_e( 'Documentation', 'envince' ); ?></a></p>
 					</div>
 
 					<div class="col">
-						<h3><?php echo esc_html_e( 'Got theme support question?', 'envince' ); ?></h3>
+						<h3><?php esc_html_e( 'Got theme support question?', 'envince' ); ?></h3>
 						<p><?php esc_html_e( 'Please put it in our dedicated support forum.', 'envince' ) ?></p>
 						<p><a href="<?php echo esc_url( 'http://themegrill.com/support-forum/' ); ?>" class="button button-secondary"><?php esc_html_e( 'Support Forum', 'envince' ); ?></a></p>
 					</div>
 
 					<div class="col">
-						<h3><?php echo esc_html_e( 'Got sales related question?', 'envince' ); ?></h3>
+						<h3><?php esc_html_e( 'Need more features?', 'envince' ); ?></h3>
+						<p><?php esc_html_e( 'Upgrade to PRO version for more exciting features.', 'envince' ) ?></p>
+						<p><a href="<?php echo esc_url( 'http://themegrill.com/themes/envince/' ); ?>" class="button button-secondary"><?php esc_html_e( 'View Pro', 'envince' ); ?></a></p>
+					</div>
+
+					<div class="col">
+						<h3><?php esc_html_e( 'Got sales related question?', 'envince' ); ?></h3>
 						<p><?php esc_html_e( 'Please send it via our sales contact page.', 'envince' ) ?></p>
 						<p><a href="<?php echo esc_url( 'http://themegrill.com/contact/' ); ?>" class="button button-secondary"><?php esc_html_e( 'Contact Page', 'envince' ); ?></a></p>
 					</div>
@@ -172,7 +211,7 @@ class Envince_Admin {
 					<div class="col">
 						<h3>
 							<?php
-							echo esc_html_e( 'Translate', 'envince' );
+							esc_html_e( 'Translate', 'envince' );
 							echo ' ' . $theme->display( 'Name' );
 							?>
 						</h3>
@@ -192,10 +231,10 @@ class Envince_Admin {
 			<div class="return-to-dashboard envince">
 				<?php if ( current_user_can( 'update_core' ) && isset( $_GET['updated'] ) ) : ?>
 					<a href="<?php echo esc_url( self_admin_url( 'update-core.php' ) ); ?>">
-						<?php is_multisite() ? esc_html_e( 'Return to Updates' ) : esc_html_e( 'Return to Dashboard &rarr; Updates' ); ?>
+						<?php is_multisite() ? esc_html_e( 'Return to Updates', 'envince' ) : esc_html_e( 'Return to Dashboard &rarr; Updates', 'envince' ); ?>
 					</a> |
 				<?php endif; ?>
-				<a href="<?php echo esc_url( self_admin_url() ); ?>"><?php is_blog_admin() ? esc_html_e( 'Go to Dashboard &rarr; Home' ) : esc_html_e( 'Go to Dashboard' ); ?></a>
+				<a href="<?php echo esc_url( self_admin_url() ); ?>"><?php is_blog_admin() ? esc_html_e( 'Go to Dashboard &rarr; Home', 'envince' ) : esc_html_e( 'Go to Dashboard', 'envince' ); ?></a>
 			</div>
 		</div>
 		<?php
@@ -246,7 +285,7 @@ class Envince_Admin {
 			$changelog .= '<pre class="changelog">';
 
 			foreach ( $changes as $index => $line ) {
-				$changelog .= wp_kses_post( preg_replace( '~(=\s###Version\s(\d+(?:\.\d+)+)\s*=|$)~Uis', '<span class="title">${1}</span>', $line ) );
+				$changelog .= wp_kses_post( preg_replace( '~(\s*###\sVersion(\d+(?:\.\d+)+)\s*=|$)~Uis', '<span class="title">${1}</span>', $line ) );
 			}
 
 			$changelog .= '</pre>';
@@ -266,17 +305,173 @@ class Envince_Admin {
 
 			<p class="about-description"><?php esc_html_e( 'This theme recommends following plugins.', 'envince' ); ?></p>
 			<ol>
-				<li><?php printf(__('<a href="%s" target="_blank">Contact Form 7</a>', 'envince'), esc_url('https://wordpress.org/plugins/contact-form-7/')); ?></li>
-				<li><?php printf(__('<a href="%s" target="_blank">Page Builder by SiteOrigin</a>', 'envince'), esc_url('https://wordpress.org/plugins/siteorigin-panels/')); ?></li>
-				<li><?php printf(__('<a href="%s" target="_blank">Social Icons</a>', 'spacious'), esc_url('https://wordpress.org/plugins/social-icons/')); ?>
+				<li><a href="<?php echo esc_url( 'https://wordpress.org/plugins/social-icons/' ); ?>" target="_blank"><?php esc_html_e( 'Social Icons', 'envince' ); ?></a>
+					<?php esc_html_e(' by ThemeGrill', 'envince'); ?>
 				</li>
-				<li><?php printf(__('<a href="%s" target="_blank">Easy Social Sharing</a>', 'spacious'), esc_url('https://wordpress.org/plugins/easy-social-sharing/')); ?>
+				<li><a href="<?php echo esc_url( 'https://wordpress.org/plugins/easy-social-sharing/' ); ?>" target="_blank"><?php esc_html_e( 'Easy Social Sharing', 'envince' ); ?></a>
+					<?php esc_html_e(' by ThemeGrill', 'envince'); ?>
 				</li>
-				<li>
-					<?php printf(__('<a href="%s" target="_blank">WMPL</a>', 'envince'), esc_url('https://wpml.org/')); ?>
-					<?php esc_html_e('Fully Compatible in Pro Version', 'envince'); ?>
+				<li><a href="<?php echo esc_url( 'https://wordpress.org/plugins/contact-form-7/' ); ?>" target="_blank"><?php esc_html_e( 'Contact Form 7', 'envince' ); ?></a></li>
+				<li><a href="<?php echo esc_url( 'https://wordpress.org/plugins/siteorigin-panels/' ); ?>" target="_blank"><?php esc_html_e( 'Page Builder by SiteOrigin', 'envince' ); ?></a></li>
+				</li>
+				<li><a href="<?php echo esc_url( 'https://wpml.org/' ); ?>" target="_blank"><?php esc_html_e( 'WPML', 'envince' ); ?></a>
+					<?php esc_html_e(' Fully Compatible in Pro', 'envince'); ?>
 				</li>
 			</ol>
+
+		</div>
+		<?php
+	}
+
+	/**
+	 * Output the free vs pro screen.
+	 */
+	public function free_vs_pro_screen() {
+		?>
+		<div class="wrap about-wrap">
+
+			<?php $this->intro(); ?>
+
+			<p class="about-description"><?php esc_html_e( 'Upgrade to PRO version for more exciting features.', 'envince' ); ?></p>
+
+			<table>
+				<thead>
+					<tr>
+						<th class="table-feature-title"><h3><?php esc_html_e('Features', 'envince'); ?></h3></th>
+						<th><h3><?php esc_html_e('Envince', 'envince'); ?></h3></th>
+						<th><h3><?php esc_html_e('Envince Pro', 'envince'); ?></h3></th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td><h3><?php esc_html_e('Google Fonts Option', 'envince'); ?></h3></td>
+						<td><span class="dashicons dashicons-no"></span></td>
+						<td><?php esc_html_e('600+', 'envince'); ?></td>
+					</tr>
+					<tr>
+						<td><h3><?php esc_html_e('Font Size options', 'envince'); ?></h3></td>
+						<td><span class="dashicons dashicons-no"></span></td>
+						<td><span class="dashicons dashicons-yes"></span></td>
+					</tr>
+					<tr>
+						<td><h3><?php esc_html_e('Color Palette', 'envince'); ?></h3></td>
+						<td><?php esc_html_e('Primary Color Option', 'envince'); ?></td>
+						<td><?php esc_html_e('20+ Color Options', 'envince'); ?></td>
+					</tr>
+					<tr>
+						<td><h3><?php esc_html_e('Social Icons', 'envince'); ?></h3></td>
+						<td><span class="dashicons dashicons-yes"></span></td>
+						<td><span class="dashicons dashicons-yes"></span></td>
+					</tr>
+					<tr>
+						<td><h3><?php esc_html_e('Boxed & Wide layout option', 'envince'); ?></h3></td>
+						<td><span class="dashicons dashicons-yes"></span></td>
+						<td><span class="dashicons dashicons-yes"></span></td>
+					</tr>
+					<tr>
+						<td><h3><?php esc_html_e('Custom Menu', 'envince'); ?></h3></td>
+						<td><?php esc_html_e('1', 'envince'); ?></td>
+						<td><?php esc_html_e('2', 'envince'); ?></td>
+					</tr>
+					<tr>
+						<td><h3><?php esc_html_e('Translation Ready', 'envince'); ?></h3></td>
+						<td><span class="dashicons dashicons-yes"></span></td>
+						<td><span class="dashicons dashicons-yes"></span></td>
+					</tr>
+					<tr>
+						<td><h3><?php esc_html_e('WPML Compatible', 'envince'); ?></h3></td>
+						<td><span class="dashicons dashicons-no"></span></td>
+						<td><span class="dashicons dashicons-yes"></span></td>
+					</tr>
+					<tr>
+						<td><h3><?php esc_html_e('The Events Calendar Compatible', 'envince'); ?></h3></td>
+						<td><span class="dashicons dashicons-no"></span></td>
+						<td><span class="dashicons dashicons-yes"></span></td>
+					</tr>
+					<tr>
+						<td><h3><?php esc_html_e('Footer Copyright Editor', 'envince'); ?></h3></td>
+						<td><span class="dashicons dashicons-no"></span></td>
+						<td><span class="dashicons dashicons-yes"></span></td>
+					</tr>
+					<tr>
+						<td><h3><?php esc_html_e('Footer Widgets Column', 'envince'); ?></h3></td>
+						<td><?php esc_html_e('1,2,3,4 Columns', 'envince'); ?></td>
+						<td><?php esc_html_e('1,2,3,4 Columns', 'envince'); ?></td>
+					</tr>
+					<tr>
+						<td><h3><?php esc_html_e('Demo Content', 'envince'); ?></h3></td>
+						<td><span class="dashicons dashicons-yes"></span></td>
+						<td><span class="dashicons dashicons-yes"></span></td>
+					</tr>
+					<tr>
+						<td><h3><?php esc_html_e('Support', 'envince'); ?></h3></td>
+						<td><?php esc_html_e('Forum', 'envince'); ?></td>
+						<td><?php esc_html_e('Forum + Emails/Support Ticket', 'envince'); ?></td>
+					</tr>
+					<tr>
+						<td><h3><?php esc_html_e('Unique Posts System', 'envince'); ?></h3></td>
+						<td><span class="dashicons dashicons-no"></span></td>
+						<td><span class="dashicons dashicons-yes"></span></td>
+					</tr>
+					<tr>
+						<td><h3><?php esc_html_e('TG: Advertisement widget', 'envince'); ?></h3></td>
+						<td><span class="dashicons dashicons-yes"></span></td>
+						<td><span class="dashicons dashicons-yes"></span></td>
+					</tr>
+					<tr>
+						<td><h3><?php esc_html_e('TG: Featured Post Slider widget', 'envince'); ?></h3></td>
+						<td><span class="dashicons dashicons-yes"></span></td>
+						<td><span class="dashicons dashicons-yes"></span></td>
+					</tr>
+					<tr>
+						<td><h3><?php esc_html_e('TG: Image Grid Post widget', 'envince'); ?></h3></td>
+						<td><span class="dashicons dashicons-yes"></span></td>
+						<td><span class="dashicons dashicons-yes"></span></td>
+					</tr>
+					<tr>
+						<td><h3><?php esc_html_e('TG: Two Column Featured Post widget', 'envince'); ?></h3></td>
+						<td><span class="dashicons dashicons-yes"></span></td>
+						<td><span class="dashicons dashicons-yes"></span></td>
+					</tr>
+					<tr>
+						<td><h3><?php esc_html_e('TG: One Column Featured Post widget', 'envince'); ?></h3></td>
+						<td><span class="dashicons dashicons-yes"></span></td>
+						<td><span class="dashicons dashicons-yes"></span></td>
+					</tr>
+					<tr>
+						<td><h3><?php esc_html_e('TG: News Ticker Widget', 'envince'); ?></h3></td>
+						<td><span class="dashicons dashicons-no"></span></td>
+						<td><span class="dashicons dashicons-yes"></span></td>
+					</tr>
+					<tr>
+						<td><h3><?php esc_html_e('TG: Post List Widget', 'envince'); ?></h3></td>
+						<td><span class="dashicons dashicons-no"></span></td>
+						<td><span class="dashicons dashicons-yes"></span></td>
+					</tr>
+					<tr>
+						<td><h3><?php esc_html_e('TG: Post Carousel Widget', 'envince'); ?></h3></td>
+						<td><span class="dashicons dashicons-no"></span></td>
+						<td><span class="dashicons dashicons-yes"></span></td>
+					</tr>
+					<tr>
+						<td><h3><?php esc_html_e('TG: Events Grid', 'envince'); ?></h3></td>
+						<td><span class="dashicons dashicons-no"></span></td>
+						<td><span class="dashicons dashicons-yes"></span></td>
+					</tr>
+					<tr>
+						<td><h3><?php esc_html_e('TG: Single Post Slider', 'envince'); ?></h3></td>
+						<td><span class="dashicons dashicons-no"></span></td>
+						<td><span class="dashicons dashicons-yes"></span></td>
+					</tr>
+					<tr>
+						<td></td>
+						<td></td>
+						<td class="btn-wrapper">
+							<a href="<?php echo esc_url( apply_filters( 'envince_pro_theme_url', 'http://themegrill.com/themes/envince/' ) ); ?>" class="button button-secondary docs" target="_blank"><?php _e( 'View Pro', 'envince' ); ?></a>
+						</td>
+					</tr>
+				</tbody>
+			</table>
 
 		</div>
 		<?php
