873867c9caa3bbee537a3c11697573594b53320a
Nitu Shrestha <shresthanitu984@gmail.com>
2018-08-01 15:09:52 +0545

    Update PHP DocBlock

--- inc/demo-importer/includes/functions-demo-config.php
+++ inc/demo-importer/includes/functions-demo-config.php
@@ -2,42 +2,40 @@
 /**
  * Functions for configuring demo importer.
  *
- * @author   ThemeGrill
- * @category Admin
- * @package  Importer/Functions
- * @version  1.0.0
+ * @package Importer/Functions
+ * @version 1.0.0
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit;
-}
+defined( 'ABSPATH' ) || exit;
 
 /**
  * Setup demo importer packages.
  *
  * @deprecated 1.5.0
  *
- * @param  array $packages
+ * @param  array $packages Demo packages.
+ *
  * @return array
  */
 function envince_demo_importer_packages( $packages ) {
 	$new_packages = array(
-		'envince-free' => array(
+		'envince-free'     => array(
 			'name'    => esc_html__( 'Envince', 'envince' ),
 			'preview' => 'https://demo.themegrill.com/envince/',
 		),
-		'envince-pro'  => array(
+		'envince-pro'      => array(
 			'name'     => __( 'Envince Pro', 'envince' ),
 			'preview'  => 'https://demo.themegrill.com/envince-pro/',
-			'pro_link' => 'https://themegrill.com/themes/envince/'
+			'pro_link' => 'https://themegrill.com/themes/envince/',
 		),
-		'envince-pro-food'  => array(
+		'envince-pro-food' => array(
 			'name'     => __( 'Envince Pro Food', 'envince' ),
 			'preview'  => 'https://demo.themegrill.com/envince-pro-food/',
-			'pro_link' => 'https://themegrill.com/themes/envince/'
-		)
+			'pro_link' => 'https://themegrill.com/themes/envince/',
+		),
 	);
 
 	return array_merge( $new_packages, $packages );
 }
+
 add_filter( 'themegrill_demo_importer_packages', 'envince_demo_importer_packages' );
