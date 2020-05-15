253ab2a7b7faec667aab91ed9f8681d191b69226
Ashesh Thapa <asheshmagar8888667@gmail.com>
2020-05-15 21:24:25 +0545

    Include - review notice message file to functions.php

--- functions.php
+++ functions.php
@@ -33,6 +33,9 @@ new Hybrid();
 if ( is_admin() ) {
 	require get_template_directory() . '/inc/admin/class-envince-admin.php';
 	require get_template_directory() . '/inc/admin/class-envince-tdi-notice.php';
+
+	// Calling in the admin area for the theme review notice.
+	require get_template_directory() . '/inc/admin/class-envince-theme-review-notice.php';
 }
 
 /* Do theme setup on the 'after_setup_theme' hook. */
