128525a02710fe7676fc555f801030d1155b2ee9
ThemeGrill <milanthapa299@gmail.com>
2020-05-21 11:06:42 +0545

    Added required CSS and enqueues CSS and JS files and renamed file

--- inc/admin/class-envince-admin.php
+++ inc/admin/class-envince-admin.php
@@ -23,6 +23,24 @@ if ( ! class_exists( 'Envince_Admin' ) ) :
 		 */
 		public function __construct() {
 			add_action( 'admin_menu', array( $this, 'admin_menu' ) );
+			add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_scripts' ) );
+		}
+
+		/**
+		 * Localize array for import button AJAX request.
+		 */
+		public function enqueue_scripts() {
+			wp_enqueue_style( 'envince-admin-style', get_template_directory_uri() . '/inc/admin/css/admin.css', array(), ENVINCE_THEME_VERSION );
+
+			wp_enqueue_script( 'envince-plugin-install-helper', get_template_directory_uri() . '/inc/admin/js/plugin-handle.js', array( 'jquery' ), ENVINCE_THEME_VERSION, true );
+
+			$welcome_data = array(
+				'uri'      => esc_url( admin_url( '/themes.php?page=demo-importer&browse=all&envince-hide-notice=welcome' ) ),
+				'btn_text' => esc_html__( 'Processing...', 'envince' ),
+				'nonce'    => wp_create_nonce( 'envince_demo_import_nonce' ),
+			);
+
+			wp_localize_script( 'envince-plugin-install-helper', 'envinceRedirectDemoPage', $welcome_data );
 		}
 
 		/**
@@ -41,15 +59,7 @@ if ( ! class_exists( 'Envince_Admin' ) ) :
 					'welcome_screen',
 				)
 			);
-			add_action( 'admin_print_styles-' . $page, array( $this, 'enqueue_styles' ) );
-		}
-
-		/**
-		 * Enqueue styles.
-		 */
-		public function enqueue_styles() {
-
-			wp_enqueue_style( 'envince-welcome', get_template_directory_uri() . '/css/admin/welcome.css', array(), ENVINCE_THEME_VERSION );
+			add_action( 'admin_print_styles-' . $page, array( $this, 'enqueue_scripts' ) );
 		}
 
 		/**
