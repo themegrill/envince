bfaf9c1fc23a01c1fbedfe1f1df43630fd647560
ankitkarki17 <ankitkarki17@gmail.com>
2021-06-29 18:34:34 +0545

    Refactor code

--- inc/admin/class-envince-dashboard.php
+++ inc/admin/class-envince-dashboard.php
@@ -40,10 +40,16 @@ class Envince_Dashboard {
 		/* translators: %s: Theme Name. */
 		$theme_page_name = sprintf( esc_html__( '%s Options', 'envince' ), $theme->Name );
 
-		$page = add_theme_page( $theme_page_name, $theme_page_name, 'edit_theme_options', 'envince-options', array(
-			$this,
-			'option_page'
-		) );
+		$page = add_theme_page(
+			$theme_page_name,
+			$theme_page_name,
+			'edit_theme_options',
+			'envince-options',
+			array(
+				$this,
+				'option_page',
+			)
+		);
 
 		add_action( 'admin_print_styles-' . $page, array( $this, 'enqueue_styles' ) );
 	}
