564f7d84f5bfb37209c50eb12293388d1c486932
som shrestha <109511507+som3669@users.noreply.github.com>
2025-08-12 13:33:47 +0545

    Merge pull request #55 from themegrill/TT-3580-envince-vulnerability-check
    fix security capability issue

--- inc/admin/class-envince-admin.php
+++ inc/admin/class-envince-admin.php
@@ -36,8 +36,13 @@ if ( ! class_exists( 'Envince_Admin' ) ) :
 			$welcome_data = array(
 				'uri'      => esc_url( admin_url( '/themes.php?page=demo-importer&browse=all&envince-hide-notice=welcome' ) ),
 				'btn_text' => esc_html__( 'Processing...', 'envince' ),
-				'nonce'    => wp_create_nonce( 'envince_demo_import_nonce' ),
 			);
+
+			// Only add nonce and ajaxurl if user has appropriate capabilities
+			if ( current_user_can( 'manage_options' ) ) {
+				$welcome_data['nonce']   = wp_create_nonce( 'envince_demo_import_nonce' );
+				$welcome_data['ajaxurl'] = admin_url( 'admin-ajax.php' );
+			}
 
 			wp_localize_script( 'envince-plugin-install-helper', 'envinceRedirectDemoPage', $welcome_data );
 		}
